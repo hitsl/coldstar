9a105167fee820ec52b49c035dcca33e7aedba2c
viruzzz-kun <viruzzz.soft@gmail.com>
2014-10-13 12:49:14 +0400

    Fixe

---
+++ coldstar.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CWD=`pwd`
+
+VIRTUALENV_PATH=$CWD/venv
+
+if [ ! -d "$VIRTUALENV_PATH" ]
+then
+    echo "Virtualenv not created. trying creating virtualenv..."
+    virtualenv venv || (echo "Virtualenv is not installed" && exit -1)
+    . $VIRTUALENV_PATH/bin/activate
+    pip install -r ./requirements.txt || (echo "Virtualenv creation unsuccessful" && exit -1)
+else
+    echo "Activating virtualenv"
+    . $VIRTUALENV_PATH/bin/activate
+fi
+
+echo "Starting Coldstar..."
+
+PYTHONPATH=$CWD twistd -n coldstar
+
+deactivate
