f4703f059ac4bf5d223ff41d308dcdba14e4eaf3
viruzzz-kun <viruzzz.soft@gmail.com>
2014-10-16 11:13:02 +0400

    Унёс создание сервиса в плагин

--- twisted/plugins/coldstar_plugin.py
+++ twisted/plugins/coldstar_plugin.py
@@ -7,8 +7,6 @@ from twisted.python import usage
 from twisted.application.service import IServiceMaker
 from twisted.plugin import IPlugin
 
-import coldstar
-
 __author__ = 'viruzzz-kun'
 __created__ = '13.09.2014'
 
@@ -33,7 +31,45 @@ class MyServiceMaker(object):
     options = Options
 
     def makeService(self, config):
-        return coldstar.makeService(config)
+        from autobahn.twisted.resource import WebSocketResource
+        from twisted.application import internet
+        from twisted.web.resource import IResource, Resource
+        from twisted.web.server import Site
+        from coldstar.interfaces import IRestService, IWsLockFactory
+
+        from coldstar.service import ColdStarService
+        from coldstar.test_page import TestPageResource
+
+        # noinspection PyUnresolvedReferences
+        from coldstar import rest, ws, session
+
+        cold_star = ColdStarService()
+        cold_star.short_timeout = int(config.get('tmp-lock-timeout', 60))
+        cold_star.long_timeout = int(config.get('lock-timeout', 3600))
+
+        rest_service = IRestService(cold_star)
+        rest_resource = IResource(rest_service)
+
+        ws_factory = IWsLockFactory(cold_star)
+        ws_resource = WebSocketResource(ws_factory)
+
+        test_page_resource = TestPageResource()
+
+        root_resource = Resource()
+        root_resource.putChild('lock', rest_resource)
+        root_resource.putChild('ws', ws_resource)
+        root_resource.putChild('test', test_page_resource)
+
+        site = Site(root_resource)
+
+        web_service = internet.TCPServer(
+            int(config.get('port', 5000)),
+            site,
+            interface=config.get('interface', '0.0.0.0')
+        )
+        web_service.setServiceParent(cold_star)
+
+        return cold_star
 
 
 serviceMaker = MyServiceMaker()
