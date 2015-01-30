0c303306006cd815cba6689862d5531c6301053f
viruzzz-kun <viruzzz.soft@gmail.com>
2015-01-30 14:52:56 +0300

    bootstrap.sh update

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,23 +2,25 @@
 
 CWD=`pwd`
 
-VIRTUALENV_PATH=$CWD/venv
-
-if [ ! -d "$VIRTUALENV_PATH" ]
+VIRTUALENV_PATH=${CWD}/venv
+if [ ! -d "${VIRTUALENV_PATH}" ]
 then
-    [ ! -e config.yaml ] && cp config_dist.yaml config.yaml
-    echo "Virtualenv not created. trying creating virtualenv..."
+    if [ $1 ]
+    then
+        VARIANT=$1
+    else
+        VARIANT=mysql
+    fi
+    echo "Virtualenv not created. trying creating virtualenv for '${VARIANT}'..."
+    REQUIREMENTS_FILE="./requirements/${VARIANT}.txt"
+    if [ ! -e ${REQUIREMENTS_FILE} ]
+    then
+        echo "${REQUIREMENTS_FILE} does not exist"
+        exit -1
+    fi
     virtualenv venv || (echo "Virtualenv is not installed" && exit -1)
-    . $VIRTUALENV_PATH/bin/activate
-    pip install -r ./requirements.txt || (echo "Virtualenv creation unsuccessful" && exit -1)
+    . ${VIRTUALENV_PATH}/bin/activate
+    pip install -r ${REQUIREMENTS_FILE} || (echo "Virtualenv creation unsuccessful" && exit -1)
     patch -p1 -d venv/lib/python2.7/site-packages < twisted.patch.diff
-else
-    echo "Activating virtualenv"
-    . $VIRTUALENV_PATH/bin/activate
+    deactivate
 fi
-
-# echo "Starting Coldstar..."
-
-# PYTHONPATH=$CWD twistd -n coldstar
-
-deactivate
